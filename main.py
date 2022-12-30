45ec8819604ca0b451e2f434efbfc492f1b983e8
omeratifyasar <ysrankr06@gmail.com>
2022-12-30 09:31:00 +0300

    2.ci deneme

--- main.py
+++ main.py
@@ -1,47 +1,36 @@
-#Most Similar Rafine
+from gensim.models import KeyedVectors
 import tkinter as tk
 
-window = tk.Tk()
-window.title("Basit Pencere")
-window.geometry("300x200")
-
-label = tk.Label(window, text="İyi Aramalar :D!")
+root = tk.Tk()
+root.geometry("500x300")
+root.title("Kelime Arama Ve Rafine işlemine Hoşgeldiniz :D")
+entry = tk.Entry(root, bg = "black", fg = "white")
+entry.pack()
+label = tk.Label(root, text="Ekrandaki siyah alana aramk istediğniz 1 veya 2 anahtar kelime yazınız.")
 label.pack()
-
-window.mainloop()
-
-
-from gensim.models import KeyedVectors
 
 print("Model Yükleniyor...")
 kelimeVektoru = KeyedVectors.load_word2vec_format('trModel100', binary=True)
 print(kelimeVektoru)
 
 def benzerKelimeler():
+    anahtarKelimeler = entry.get().split()
+    print("Girdiğiniz optimize edilecek kelimeler: " + " ".join(anahtarKelimeler))
+    if anahtarKelimeler:
+        try:
+            oneriler = kelimeVektoru.most_similar(positive=anahtarKelimeler)
+            for oneri in oneriler:
+                if not any(keyword in oneri[0] for keyword in anahtarKelimeler):
+                    print(oneri[0])
+                    print("https://www.google.com.tr/search?q=" + oneri[0])
+        except KeyError:
+            print("Girdiğiniz anahtar kelime bulunamadı lütfen başka bir kelime giriniz.")
 
-    anahtarKelime = input("Anahtar Kelime: ").lower()
-    print("Girilen Kelime : "+ str(anahtarKelime))
-    oneriler = (kelimeVektoru.most_similar(positive=anahtarKelime))
-    print(oneriler)
+def quit():
+    root.destroy()
 
-    def benzerKelimeler2():
-
-        anahtarKelime2 = input("Anahtar Kelime: ").lower()
-        print("Girilen Kelime : " + str(anahtarKelime))
-        oneriler = (kelimeVektoru.most_similar(positive=anahtarKelime))
-        print(oneriler)
-
-    # Önerileri rafine et
-    for oneri in oneriler:
-        if anahtarKelime not in oneri[0]:
-            print("https://www.google.com.tr/search?q=" + oneri[0])
-
-    benzerKelimeler()
-
-benzerKelimeler()
-
-
-
-
-
-
+button1 = tk.Button(root, text="Anahtar Kelimeleri Al", command=benzerKelimeler)
+button1.pack()
+button2 = tk.Button(root, text="Çıkış", command=quit)
+button2.pack()
+root.mainloop()
